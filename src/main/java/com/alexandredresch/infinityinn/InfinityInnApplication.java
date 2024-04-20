2f8d4ab05933d0117be4c7ea8a683ccc22dd21ed
AlexandreDresch <connectWithAlexandre@proton.me>
2024-04-20 15:42:53 -0300

    config: exclude SecurityAutoConfiguration

--- src/main/java/com/alexandredresch/infinityinn/InfinityInnApplication.java
+++ src/main/java/com/alexandredresch/infinityinn/InfinityInnApplication.java
@@ -2,8 +2,9 @@ package com.alexandredresch.infinityinn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class InfinityInnApplication {
 
 	public static void main(String[] args) {
