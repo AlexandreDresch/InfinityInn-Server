5e7953c6da8a4e0a00f0387965cea8f76130198f
AlexandreDresch <connectWithAlexandre@proton.me>
2024-04-25 23:57:00 -0300

    feat: add getAllRooms & getRoomPhotoByRoomId methods, add ResourceNotFoundException

--- src/main/java/com/alexandredresch/infinityinn/service/RoomServiceImpl.java
+++ src/main/java/com/alexandredresch/infinityinn/service/RoomServiceImpl.java
@@ -1,5 +1,6 @@
 package com.alexandredresch.infinityinn.service;
 
+import com.alexandredresch.infinityinn.exception.ResourceNotFoundException;
 import com.alexandredresch.infinityinn.model.Room;
 import com.alexandredresch.infinityinn.repository.RoomRepository;
 import lombok.RequiredArgsConstructor;
@@ -12,6 +13,7 @@ import java.math.BigDecimal;
 import java.sql.Blob;
 import java.sql.SQLException;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -33,5 +35,24 @@ public class RoomServiceImpl implements IRoomService {
     @Override
     public List<String> getAllRoomTypes() {
         return roomRepository.findDistinctRoomTypes();
+    }
+
+    @Override
+    public List<Room> getAllRooms() {
+        return roomRepository.findAll();
+    }
+
+    @Override
+    public byte[] getRoomPhotoByRoomId(Long roomId) throws SQLException {
+        Optional<Room> selectedRoom = roomRepository.findById(roomId);
+        if (selectedRoom.isEmpty()){
+            throw new ResourceNotFoundException("Unable to find room with given Id.");
+        }
+        Blob photoBlob = selectedRoom.get().getPhoto();
+        if (photoBlob != null) {
+            return photoBlob.getBytes(1, (int) photoBlob.length());
+        }
+
+        return null;
     }
 }
